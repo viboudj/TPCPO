8dc8597168adf1b40085bdf8892ed32ea3c7c607
Jean-Baptiste <Jean-Baptiste@Jean-Baptiste>
2015-01-12 14:00:45 +0100

    initialisation

---
+++ IHM-memory/src/main/java/fr/ensma/ia/tp_IHM_memory/IHM_memory/App.java
@@ -0,0 +1,13 @@
+package fr.ensma.ia.tp_IHM_memory.IHM_memory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
