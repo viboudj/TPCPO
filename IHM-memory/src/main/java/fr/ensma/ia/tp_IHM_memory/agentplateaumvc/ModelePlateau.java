11dafc14839d4e854982e17c08507581b2cd343f
douaultc <clementdouault@192.168.2.1>
2015-01-21 14:31:54 +0100

    commencement de plateau

--- IHM-memory/src/main/java/fr/ensma/ia/tp_IHM_memory/agentplateaumvc/ModelePlateau.java
+++ IHM-memory/src/main/java/fr/ensma/ia/tp_IHM_memory/agentplateaumvc/ModelePlateau.java
@@ -2,8 +2,26 @@ package fr.ensma.ia.tp_IHM_memory.agentplateaumvc;
 
 import java.util.ArrayList;
 
+import fr.ensma.ia.tp_IHM_memory.agentcartemvc.ModeleCarte;
+import fr.ensma.ia.tpmemomry.tpcpo1.Partie;
+
 public class ModelePlateau {
 
+	/**
+	 * Reference du noyau fonctionnel du memory
+	 */
+	private Partie noyauFonctionnel;
+	
+	/**
+	 * Nombre de cartes sur le plateau
+	 */
+	private int nbCartesSurPlateau;
+	
+	/**
+	 * Liste melangee des cartes composant le plateau
+	 */
+	private ArrayList<ModeleCarte> listeCartesSurPlateau;
+	
 	/**
 	 * Liste des observateurs du plateau
 	 */
